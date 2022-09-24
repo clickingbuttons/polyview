3b1dbe88d823ed366bedeae3e89497e917bc461f
clickingbuttons <clickingbuttons@pm.me>
2022-09-24 17:11:55 -0400

    better update message

--- build/constants.mjs
+++ build/constants.mjs
@@ -1,6 +1,9 @@
 export function getTime() {
 	const now = new Date();
-	return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+	const hours = String(now.getHours()).padStart(2, '0');
+	const minutes = String(now.getMinutes()).padStart(2, '0');
+	const seconds = String(now.getSeconds()).padStart(2, '0'); 
+	return `${hours}:${minutes}:${seconds}`;
 }
 export const outdir = 'dist';
 export const isProd = process.env.NODE_ENV == 'production';
