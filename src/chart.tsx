3ebc9ecb251ced887abd424b4e3f361b67d8584e
zack <43246297+clickingbuttons@users.noreply.github.com>
2022-09-27 17:57:00 -0400

    dark mode, cleanup chart.tsx

--- src/chart.tsx
+++ src/chart.tsx
@@ -1,6 +1,6 @@
 import { CandlestickData, createChart, UTCTimestamp, ChartOptions, DeepPartial, IChartApi, ISeriesApi } from 'lightweight-charts';
-import { restClient, websocketClient } from '@polygon.io/client-js';
-import { useRef, useEffect, useState } from 'preact/hooks';
+import { restClient, websocketClient, IRestClient } from '@polygon.io/client-js';
+import { useRef, useEffect, useState, useMemo } from 'preact/hooks';
 import { Toolbar, Timespan } from './toolbar';
 import './chart.css';
 
@@ -36,61 +36,107 @@ function getTimespanMS(timespan: Timespan): number {
 }
 
 const barsPageSize = 10000;
+async function loadData(rest: IRestClient, ticker: string, multiplier: number, timespan: string, date: string | number, loadForwards: boolean): Promise<CandlestickData[]> {
+	// Cleverness: desc + reverse
+	const from = loadForwards ? String(date) : '1970-01-01';
+	const to = loadForwards ? '2100-01-01' : String(date);
+	return rest.stocks.aggregates(ticker, multiplier, timespan, from, to, { limit: barsPageSize, sort: loadForwards ? 'asc' : 'desc' })
+		.then(res => {
+			if (!res.results)
+				return [];
+
+			return res.results.map(bar => ({
+				// for candlestick series
+				time: getLocalTime(bar.t),
+				open: bar.o,
+				high: bar.h,
+				low: bar.l,
+				close: bar.c,
+
+				// for histogram series (also uses time)
+				value: bar.v,
+				color: bar.o < bar.c ? 'rgba(0, 150, 136, 0.8)' : 'rgba(255,82,82, 0.8)',
+			} as CandlestickData))
+		})
+		.then(candles => loadForwards ? candles : candles.reverse());
+}
+
+async function getWSTicker(rest: IRestClient, ticker: string): Promise<string> {
+	if (ticker.startsWith('X')) {
+		// ...polygon only allows XT:X:BTC-USD and not XT:X:BTCUSD :(
+		return rest.reference.tickerDetails(ticker)
+			.then(ticker => {
+				const { base_currency_symbol, currency_symbol } = ticker.results as any;
+				return `X:${base_currency_symbol}-${currency_symbol}`
+			})
+	} else {
+		return `${ticker}`
+	}
+}
 
 export function Chart({ apiKey }) {
+	// lightweight-charts
 	const div = useRef();
-	const rest = restClient(apiKey);
-	let [chart, setChart] = useState(null as IChartApi);
-	let [series, setSeries] = useState([] as ISeriesApi<any>[]);
-	const [settings, setSettings] = useState({} as DeepPartial<ChartOptions>);
-	const [live, setLive] = useState(false);
+	const [chart, setChart] = useState(null as IChartApi);
+	const [options, setOptions] = useState({} as DeepPartial<ChartOptions>);
+	const [series, setSeries] = useState([] as ISeriesApi<any>[]);
+
+	useEffect(() => {
+		setChart(createChart(div.current, {
+			timeScale: {
+				timeVisible: true,
+				secondsVisible: false,
+			},
+			localization: {
+				locale: 'en',
+				dateFormat: 'yyyy-MM-dd',
+			},
+		}));
+		function onResize() {
+			const ele = div.current as HTMLDivElement;
+			// Bit of a hack since we know only chart + toolbar are on page.
+			// Not sure a better way to handle resizing from tall to short since the 
+			// chart decides to keep its height larger than its flexbox parent.
+			const app = document.getElementById('app');
+			const toolbar = document.getElementById('toolbar');
+			chart.resize(ele.offsetWidth, app.offsetHeight - toolbar.offsetHeight, true);
+		}
+		window.addEventListener('resize', onResize);
+		return () => window.removeEventListener('resize', onResize);
+	}, []);
+
+	useEffect(() => {
+		if (!options || Object.keys(options).length === 0) {
+			return;
+		}
+		chart.applyOptions(options);
+	}, [options]);
+
+	// data
+	const rest = useMemo(() => restClient(apiKey), [apiKey]);
+	const [data, setData] = useState([] as CandlestickData[]);
+	const [hasNewSeries, setHasNewSeries] = useState(false);
+	// data picker
 	const [ticker, setTicker] = useState('AAPL');
 	const [multiplier, setMultiplier] = useState(1);
 	const [timespan, setTimespan] = useState('minute' as Timespan);
 	const [date, setDate] = useState(toymd(new Date()));
-	const [data, setData] = useState([] as CandlestickData[]);
-	const [scrollToStart, setScrollToStart] = useState(false);
-
-	async function loadData(ticker: string, multiplier: number, timespan: string, date: string | number, loadForwards: boolean): Promise<CandlestickData[]> {
-		// Cleverness: desc + reverse
-		const from = loadForwards ? String(date) : '1970-01-01';
-		const to = loadForwards ? '2100-01-01' : String(date);
-		return rest.stocks.aggregates(ticker, multiplier, timespan, from, to, { limit: barsPageSize, sort: loadForwards ? 'asc' : 'desc' })
-			.then(res => {
-				if (!res.results)
-					return [];
-
-				return res.results.map(bar => ({
-					// for candlestick series
-					time: getLocalTime(bar.t),
-					open: bar.o,
-					high: bar.h,
-					low: bar.l,
-					close: bar.c,
-
-					// for histogram series (also uses time)
-					value: bar.v,
-					color: bar.o < bar.c ? 'rgba(0, 150, 136, 0.8)' : 'rgba(255,82,82, 0.8)',
-				} as CandlestickData))
-			})
-			.then(candles => loadForwards ? candles : candles.reverse());
-	}
 
 	useEffect(() => {
 		series.forEach(s => chart.removeSeries(s));
 		setSeries([]);
-		loadData(ticker, multiplier, timespan, date, false)
+		loadData(rest, ticker, multiplier, timespan, date, false)
 			.then(candles => {
 				setData(candles);
-				setScrollToStart(true);
+				setHasNewSeries(true);
 			});
 	}, [ticker, multiplier, timespan, date]);
 
 	useEffect(() => {
-		if (data.length == 0) {
+		if (data.length === 0) {
 			return;
 		}
-		if (series.length == 0) {
+		if (series.length === 0) {
 			series.push(chart.addCandlestickSeries());
 			series.push(chart.addHistogramSeries({
 				lastValueVisible: false,
@@ -104,21 +150,15 @@ export function Chart({ apiKey }) {
 		}
 		series[0].setData(data);
 		series[1].setData(data);
-		if (scrollToStart) {
-			chart.timeScale().scrollToRealTime();
-			setScrollToStart(false);
+		if (hasNewSeries) {
+			chart.timeScale().fitContent();
+			setHasNewSeries(false);
+			setReachedEnd(false);
 		}
 	}, [data]);
 
-	function onResize() {
-		const ele = div.current as HTMLDivElement;
-		// Bit of a hack since we know only chart + toolbar are on page.
-		// Not sure a better way to handle resizing from tall to short since the 
-		// chart decides to keep its height larger than its flexbox parent.
-		const app = document.getElementById('app');
-		const toolbar = document.getElementById('toolbar');
-		chart.resize(ele.offsetWidth, app.offsetHeight - toolbar.offsetHeight, true);
-	}
+	// infinite scrolling back
+	const [reachedEnd, setReachedEnd] = useState(false);
 
 	useEffect(() => {
 		if (!chart) {
@@ -126,7 +166,7 @@ export function Chart({ apiKey }) {
 		}
 		let isLoading = false;
 		function onRangeChange() {
-			if (isLoading || data.length === 0 || series.length === 0) {
+			if (isLoading || data.length === 0 || series.length === 0 || reachedEnd) {
 				return;
 			}
 			const logicalRange = chart.timeScale().getVisibleLogicalRange();
@@ -143,7 +183,7 @@ export function Chart({ apiKey }) {
 			let epochMS: number;
 			if (loadBackwards) {
 				epochMS = data[0].time as number * 1000 + getLocalOffsetMS() - timespanMS;
-				//console.log('loading more bars before', new Date(epochMS).toISOString());
+				// console.log('loading more bars before', new Date(epochMS).toISOString());
 			} else {
 				// TODO: get axis to not snap to newest date on load (which forces more data
 				// to have to be loaded)
@@ -154,158 +194,96 @@ export function Chart({ apiKey }) {
 				//console.log('need more bars after', new Date(epochMS).toISOString());
 			}
 
-			loadData(ticker, multiplier, timespan, epochMS, loadForwards)
+			loadData(rest, ticker, multiplier, timespan, epochMS, loadForwards)
 				.then(newData => {
-					//console.log('loaded', newData.length, 'more bars');
+					// console.log('loaded', newData.length, 'more bars', newData);
+					isLoading = false;
+					if (newData.length === 0) {
+						setReachedEnd(true);
+					}
 					if (loadBackwards) {
 						setData([...newData, ...data]);
 					} else {
 						setData([...data, ...newData]);
 					}
-					isLoading = false;
 					chart.applyOptions({ handleScroll: true });
 				});
 		}
 		chart.timeScale().subscribeVisibleLogicalRangeChange(onRangeChange);
 
 		return () => chart.timeScale().unsubscribeVisibleLogicalRangeChange(onRangeChange);
-	}, [ticker, multiplier, timespan, data]);
+	}, [ticker, multiplier, timespan, data, reachedEnd]);
 
+
+	// live
+	const [live, setLive] = useState(false);
 	useEffect(() => {
-		if (!div.current) {
+		if (!live) {
 			return;
 		}
-		const isDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-		const darkTheme = {
-			layout: {
-				backgroundColor: '#2B2B43',
-				lineColor: '#2B2B43',
-				textColor: '#D9D9D9',
-			},
-			crosshair: {
-				color: '#758696',
-			},
-			grid: {
-				vertLines: {
-					color: '#2B2B43',
-				},
-				horzLines: {
-					color: '#363C4E',
-				},
-			},
-		} as DeepPartial<ChartOptions>;
-		const lightTheme = {
-			layout: {
-				backgroundColor: '#FFFFFF',
-				lineColor: '#2B2B43',
-				textColor: '#191919',
-			},
-			grid: {
-				vertLines: {
-					visible: false,
-				},
-				horzLines: {
-					color: '#f0f3fa',
-				},
-			},
-		} as DeepPartial<ChartOptions>;
-		chart = createChart(div.current, {
-			...(isDarkMode ? darkTheme: lightTheme),
-			...(settings),
-			timeScale: {
-				timeVisible: true,
-				secondsVisible: false,
-			},
-			localization: {
-				locale: 'en',
-				dateFormat: 'yyyy-MM-dd',
-			},
-		});
-		setChart(chart);
+		console.log('live');
+		const ws = websocketClient(apiKey);
+		let client: WebSocket;
+		let topic: string;
 
-		window.addEventListener('resize', onResize);
-		return () => window.removeEventListener('resize', onResize);
-	}, []);
-
-	useEffect(() => {
-		chart.applyOptions(settings);
-	}, [settings]);
-
-	useEffect(() => {
-		if (live) {
-			console.log('live');
-			const ws = websocketClient(apiKey);
-			let client: WebSocket;
-			let topic: string;
-
-			if (ticker.startsWith('X:')) {
-				client = ws.crypto();
-				topic = 'XA';
-			} else if (ticker.startsWith('O:')) {
-				client = ws.options();
-				topic = 'AM';
-			} else if (ticker.startsWith('C:')) {
-				client = ws.forex();
-				topic = 'CA';
-			} else {
-				client = ws.stocks();
-				topic = 'AM';
-			}
-
-			client.onmessage = ({ data }) => {
-				const [message] = JSON.parse(data);
-				// console.log('message', message);
-
-				switch (message.ev) {
-					case 'status':
-						if (message.status == 'auth_success') {
-							if (topic == 'XA') {
-								// ...what a dumb decision to only allow XT:X:BTC-USD and not XT:X:BTCUSD
-								rest.reference.tickerDetails(ticker)
-									.then(ticker => {
-										const { base_currency_symbol, currency_symbol } = ticker.results as any;
-										client.send(JSON.stringify({
-											action: "subscribe",
-											params: `${topic}.X:${base_currency_symbol}-${currency_symbol}`
-										}));
-									})
-							} else {
-								client.send(JSON.stringify({
-									action: "subscribe",
-									params: `${topic}.${ticker}`
-								}));
-							}
-						}
-						break;
-					default:
-						console.log('agg', message);
-						series[0].update({
-							time: getLocalTime(message.s),
-							open: message.o,
-							high: message.h,
-							low: message.l,
-							close: message.c,
-						});
-						series[1].update({
-							time: getLocalTime(message.s),
-							value: message.v,
-							color: message.o < message.c ? 'rgba(0, 150, 136, 0.8)' : 'rgba(255,82,82, 0.8)',
-						});
-						break;
-				}
-			};
-
-			return () => {
-				console.log('not live');
-				client.close();
-			};
+		if (ticker.startsWith('X:')) {
+			client = ws.crypto();
+			topic = 'XA';
+		} else if (ticker.startsWith('O:')) {
+			client = ws.options();
+			topic = 'AM';
+		} else if (ticker.startsWith('C:')) {
+			client = ws.forex();
+			topic = 'CA';
+		} else {
+			client = ws.stocks();
+			topic = 'AM';
 		}
+
+		client.onmessage = ({ data }) => {
+			const [message] = JSON.parse(data);
+			// console.log('message', message);
+
+			switch (message.ev) {
+				case 'status':
+					if (message.status == 'auth_success') {
+						getWSTicker(rest, ticker).then(ticker => {
+							console.log('subbing to', ticker);
+							client.send(JSON.stringify({
+								action: 'subscribe',
+								params: `${topic}.${ticker}`
+							}))
+						});
+					}
+					break;
+				default:
+					// console.log('agg', message);
+					series[0].update({
+						time: getLocalTime(message.s),
+						open: message.o,
+						high: message.h,
+						low: message.l,
+						close: message.c,
+					});
+					series[1].update({
+						time: getLocalTime(message.s),
+						value: message.v,
+						color: message.o < message.c ? 'rgba(0, 150, 136, 0.8)' : 'rgba(255,82,82, 0.8)',
+					});
+					break;
+			}
+		};
+
+		return () => {
+			console.log('not live');
+			client.close();
+		};
 	}, [ticker, live]);
 
 	return (
 		<>
 			<Toolbar
-				setSettings={setSettings}
+				setOptions={setOptions}
 				live={live}
 				setLive={setLive}
 				ticker={ticker}
--- src/toolbar.tsx
+++ src/toolbar.tsx
@@ -1,11 +1,45 @@
 import { PriceScaleMode, DeepPartial, ChartOptions } from 'lightweight-charts';
 import { useState, useEffect } from 'preact/hooks';
+import './toolbar.css';
 
 export type Timespan = 'minute' | 'hour' | 'day' | 'week' | 'month' | 'quarter' | 'year';
 const timespans: Timespan[] = ['minute', 'hour', 'day', 'week', 'month', 'quarter', 'year'];
+const darkTheme = {
+	layout: {
+		backgroundColor: '#2B2B43',
+		lineColor: '#2B2B43',
+		textColor: '#D9D9D9',
+	},
+	crosshair: {
+		color: '#758696',
+	},
+	grid: {
+		vertLines: {
+			color: '#2B2B43',
+		},
+		horzLines: {
+			color: '#363C4E',
+		},
+	},
+} as DeepPartial<ChartOptions>;
+const lightTheme = {
+	layout: {
+		backgroundColor: '#FFFFFF',
+		lineColor: '#2B2B43',
+		textColor: '#191919',
+	},
+	grid: {
+		vertLines: {
+			visible: false,
+		},
+		horzLines: {
+			color: '#f0f3fa',
+		},
+	},
+} as DeepPartial<ChartOptions>;
 
 export function Toolbar({
-	setSettings,
+	setOptions,
 	live,
 	setLive,
 	ticker,
@@ -18,9 +52,11 @@ export function Toolbar({
 	setDate
 }) {
 	const [percent, setPercent] = useState(false);
+	const [dark, setDark] = useState(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches);
 
 	useEffect(() => {
-		setSettings({
+		setOptions({
+			...(dark ? darkTheme : lightTheme),
 			rightPriceScale: {
 				mode: percent ? PriceScaleMode.Percentage : PriceScaleMode.Normal,
 				scaleMargins: {
@@ -37,7 +73,7 @@ export function Toolbar({
 				}
 			},
 		} as DeepPartial<ChartOptions>);
-	}, [percent]);
+	}, [percent, dark]);
 
 	return (
 		<div id="toolbar">
@@ -49,11 +85,17 @@ export function Toolbar({
 				)}
 			</select>
 			<input value={date} onChange={ev => setDate(ev.target.value)} />
+
+			<div class="toolbar-spacer" />
+
 			<button onClick={() => setPercent(!percent)}>
-				{(percent) ? <b>%</b> : '%'}
+				{percent ? <b>%</b> : '%'}
 			</button>
 			<button onClick={() => setLive(!live)}>
-				{(live) ? <b>L</b> : 'L'}
+				{live ? <b>L</b> : 'L'}
+			</button>
+			<button onClick={() => setDark(!dark)}>
+				{dark ? <b>D</b> : 'D'}
 			</button>
 		</div>
 	);
